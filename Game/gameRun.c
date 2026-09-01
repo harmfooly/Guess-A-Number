09917be2e2963cfafb5bf8c0697efeecd04343d6
anto_hf <thegreatantonio2@gmail.com>
2026-09-01 17:42:43 +0000

    Added new C information

---
+++ Game/gameRun.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	// establishing initial variables
+	int randomNumber = 0;
+	int insertNumber = 0;
+	char firstName[20];
+	char correct[20];
+	
+// Step one
+// Step two
+// Step three
+// Step four
+// Step five
+// Step six
+// Step seven
+// Step eight
+// Step nine
+	
+}
