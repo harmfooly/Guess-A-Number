73d38aa7292ab6d86924e7f90f0066fdb12ed3a5
anto_hf <thegreatantonio2@gmail.com>
2026-09-01 17:44:39 +0000

    edited gameRun

--- Game/gameRun.c
+++ Game/gameRun.c
@@ -7,7 +7,10 @@ int main(){
 	int insertNumber = 0;
 	char firstName[20];
 	char correct[20];
-	
+	/* strcpy(correct, "insert random number")
+	 * This will be replaced with the randomly generated number
+	 */
+
 // Step one
 // Step two
 // Step three
