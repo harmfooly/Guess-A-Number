2eb97a53c3c1673ec0ead7e4df3e41fab6cd05ba
anto_hf <thegreatantonio2@gmail.com>
2026-09-03 13:38:55 +0000

    Edits

--- Game/gameRun.c
+++ Game/gameRun.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(){
-	// establishing initial variables
-	int randomNumber = 0;
-	int insertNumber = 0;
-	char firstName[20];
-	char correct[20];
-	/* strcpy(correct, "insert random number")
-	 * This will be replaced with the randomly generated number
-	 */
+	// Establish variables
+	char userName[20];
+	int turns = 0;
+	int guess = -999;
+	int gameRun  = true;
 
-// Step one
-// Step two
-// Step three
-// Step four
-// Step five
-// Step six
-// Step seven
-// Step eight
-// Step nine
+	// Generate Random Number
+	int correctNumber = (rand() % 100) + 1;
+
+	//  Ask the user for name
+	printf("Hello Enter your first name: ");
+	scanf("%s", userName);
+
+	printf("Welcome, %s, We're going to play a game.", userName);
 	
+	while (gameRun){
+		  // Ask the user for number
+		turns++;
+		printf("Enter an integer between 1-100: ");
+		scanf("%d", &guess);
+
+		if (guess > correctNumber){
+			printf("Too high! Try again.");
+		} else if (guess < correctNumber){
+			 printf("Too low! Try again.");
+		} else {
+       			printf("Correct! it took you %d turns to get the number correct!", &turns);			
+	}
+	
+
 }
