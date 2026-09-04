de4fb2c7a509579f05e382e85a13fe05b33f16cb
anto_hf <thegreatantonio2@gmail.com>
2026-09-04 16:20:22 +0000

    finished guess-a-number game and posted to gameBeta

--- Game/gameRun.c
+++ Game/gameRun.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 int main(){
@@ -7,9 +9,10 @@ int main(){
 	char userName[20];
 	int turns = 0;
 	int guess = -999;
-	int gameRun  = true;
+	int game = true;
 
 	// Generate Random Number
+	srand(time(NULL));
 	int correctNumber = (rand() % 100) + 1;
 
 	//  Ask the user for name
@@ -18,19 +21,21 @@ int main(){
 
 	printf("Welcome, %s, We're going to play a game.", userName);
 	
-	while (gameRun){
+	while (game){
 		  // Ask the user for number
-		turns++;
-		printf("Enter an integer between 1-100: ");
+		turns++; // Increment turn counter upon correct guess
+		printf(" (Turn %d), Enter an integer between 1-100: ", turns);
 		scanf("%d", &guess);
 
 		if (guess > correctNumber){
 			printf("Too high! Try again.");
 		} else if (guess < correctNumber){
-			 printf("Too low! Try again.");
+			 printf("Too low! Try again."); // Provide feedback
 		} else {
-       			printf("Correct! it took you %d turns to get the number correct!", &turns);			
-	}
-	
-
-}
+       			printf("Correct! it took you %d turns to get the number correct!", turns);
+			game = false;
+					
+	} // Stop when value matches, provide message with number of turns.
+} // end of while loop
+	return 0;
+} // end of main
